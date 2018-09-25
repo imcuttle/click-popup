5e03c61edc689e9fe73db045a55c08a04696eb94
imcuttle <moyuyc95@gmail.com>
2018-09-25 23:57:58 +0800

    feat: fullfill ts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+<a name="1.0.3"></a>
+
+## [1.0.3](https://github.com/imcuttle/click-popup/compare/v1.0.2...v1.0.3) (2018-09-25)
+
 <a name="1.0.2"></a>
 
 ## 1.0.2 (2018-09-25)
--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@
  * The interaction about funny popup animation
  * @author imcuttle
  */
-declare const clickPopup: {}
+
+declare const clickPopup: (nodeList: Array<Node | string>, Node, options: any) => void
 
 export = clickPopup
